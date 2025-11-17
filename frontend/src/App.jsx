6f4f8bd1493bad629d280383845a3674ced33e65
kaveri372005-commits <kaveri372005@gmail.com>
2025-11-17 20:13:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect,useState } from "react";
 import io from 'socket.io-client';
 import Editor from "@monaco-editor/react"
 
-const socket = io("http://localhost:5000");
+const socket = io("https://realtime-code-editor-zsnb.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
